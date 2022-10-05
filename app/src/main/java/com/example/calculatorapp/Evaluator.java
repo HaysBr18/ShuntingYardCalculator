19ec96506785346657b2392ecb9cfce342a4a1f2
HaysBr18 <HaysBr@cwu.edu>
2022-10-05 15:57:56 -0700

    Calulator-app.

---
+++ app/src/main/java/com/example/calculatorapp/Evaluator.java
@@ -0,0 +1,319 @@
+package com.example.calculatorapp;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Evaluator {
+    /**
+     * Evaluate infix expression and return the result.
+     * @param expression the infix expression to be evaluated.
+     * @return a double containing the final result.
+     */
+    public static double calculate(char[] expression) {
+        Queue<String> output = shunt(expression);
+        return evalPfe(output);
+    }
+
+    /**
+     * Evaluate postfix expression and return the result.
+     * @param output the postfix expression to be evaluated.
+     * @return a double containing the final result.
+     */
+    private static double evalPfe(Queue<String> output) {
+        Stack<Double> numbers = new Stack<>();
+        final double MAX = Math.pow(2, 53); // Highest integer that can be stored with full precision in a double.
+
+        // Handle each element in the output until it is empty.
+        while (!output.isEmpty()) {
+            String curr = output.remove();
+
+            // If the element is a valid double within the legal range, push to stack.
+            // Otherwise, check which operator it is.
+            try {
+                numbers.push(Double.parseDouble(curr));
+
+            } catch (NumberFormatException e) {
+
+
+                double a;
+                double b;
+
+                switch (curr) {
+                    case "+":
+
+                        b = numbers.pop();
+                        a = numbers.pop();
+                        numbers.push(a + b);
+                        break;
+
+                    case "-":
+
+                        b = numbers.pop();
+                        a = numbers.pop();
+                        numbers.push(a - b);
+                        break;
+
+                    case "*":
+
+                        numbers.push(numbers.pop() * numbers.pop());
+                        break;
+
+                    case "/":
+                        b = numbers.pop();
+                        a = numbers.pop();
+
+                        if (b == 0)
+                            throw new
+                                    UnsupportedOperationException(
+                                    "Cannot divide by zero");
+                        numbers.push(a / b);
+                        break;
+
+                    case "^":
+                        b = numbers.pop();
+                        a = numbers.pop();
+
+                        numbers.push(Math.pow(a, b));
+                        break;
+
+                    case "sin":
+                        numbers.push(Math.sin(numbers.pop()));
+                        break;
+
+                    case "cos":
+                        numbers.push(Math.cos(numbers.pop()));
+                        break;
+
+                    case "tan":
+                        numbers.push(Math.tan(numbers.pop()));
+                        break;
+
+                    case "log":
+                        numbers.push(Math.log10(numbers.pop()));
+                        break;
+
+                    case "ln":
+                        numbers.push(Math.log(numbers.pop()));
+                        break;
+
+
+                }
+            }
+
+
+
+        }
+
+        // Throw exception if there are too many numbers in stack.
+        if (numbers.size() > 1)
+            throw new IllegalArgumentException("Invalid Input - Operand without operator.");
+
+        return numbers.pop();
+
+        }
+
+
+
+    /**
+     * Convert infix expression to postfix expression.
+     * @param expression the infix expression to be converted.
+     * @return a Queue of Strings representing each token of the postfix expression.
+     */
+    private static Queue<String> shunt(char[] expression) {
+        // Parse expression with (modified) Shunting-Yard algorithm by Edsger Dijkstra.
+        Stack<String> operators = new Stack<>();
+        Queue<String> output = new LinkedList<>();
+
+        for (int i = 0; i < expression.length; i++) {
+            // Stop parsing at the equals sign.
+            if (expression[i] == '=') {
+                break;
+            }
+
+            // Throw exception if ) is found before (.
+            if (expression[i] == ')') {
+                throw new IllegalArgumentException("Invalid Input - ')' without matching '('");
+            }
+
+            // While char is '0'-'9' or '.', add to token, then push all to output.
+            if (Character.isDigit(expression[i]) || expression[i] == '.') {
+                String token = "";
+                int decCount = 0;
+
+                while (i < expression.length && (Character.isDigit(expression[i]) || expression[i] == '.')) {
+                    if (expression[i] == '.') decCount++;
+                    token += expression[i];
+                    i++;
+                }
+
+                // Throw error if last digit is decimal point.
+                if (expression[i - 1] == '.') {
+                    throw new IllegalArgumentException("Invalid Input - Number ends with '.'");
+                }
+                // Throw error if token has multiple decimal points.
+                if (decCount > 1) {
+                    throw new IllegalArgumentException("Invalid Input - Number has more than one '.'");
+                }
+
+                output.add(token);
+                i--;
+                continue;
+            }
+
+            if (expression[i] == '(') {
+                // Check for implicit multiplication before parentheses.
+                if (i != 0 && Character.isDigit(expression[i - 1])) {
+                    operators.add("*");
+                }
+
+                // Evaluate everything within the parenthesis and push result to output.
+                i++;
+                String token = "";
+                int parenCount = 1;
+
+                while (true) {
+                    try {
+                        if (expression[i] == '(') parenCount++;
+                        else if (expression[i] == ')') parenCount--;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        // Throw exception if whole expression is parsed without finding ).
+                        throw new IllegalArgumentException("Invalid Input - '(' without matching ')'");
+                    }
+
+                    // Stop iterating when all ( are matched with ).
+                    if (parenCount == 0) break;
+
+                    token += expression[i];
+                    i++;
+                }
+
+                // Add solution to sub-expression in place of the parentheses.
+                output.add(calculate(token.toCharArray()) + "");
+
+                // Check for implicit multiplication after parentheses.
+                if (i + 1 < expression.length && (Character.isDigit(expression[i + 1]) || expression[i + 1] == '.')) {
+                    operators.add("*");
+                }
+                continue;
+            }
+
+            // Handle addition.
+            if (expression[i] == '+') {
+                if (!operators.isEmpty()) {
+                    String o = operators.peek();
+                    while (o.equals("+") || o.equals("-") || o.equals("*") || o.equals("/") || o.equals("^")) {
+                        output.add(operators.pop());
+                        if (operators.isEmpty()) break;
+                        o = operators.peek();
+                    }
+                }
+                operators.push("+");
+                continue;
+            }
+
+            // Handle subtraction/unary negation.
+            if (expression[i] == '-') {
+                // Check for unary negation, then treat it as multiplying by -1.
+                if (i == 0 || expression[i - 1] == '(' || expression[i - 1] == '+' || expression[i - 1] == '-'
+                        || expression[i - 1] == '*' || expression[i - 1] == '/' || expression[i - 1] == '^') {
+                    operators.push("*");
+                    output.add("-1");
+                } else { // Handle subtraction.
+                    if (!operators.isEmpty()) {
+                        String o = operators.peek();
+                        while (o.equals("+") || o.equals("-") || o.equals("*") || o.equals("/") || o.equals("^")) {
+                            output.add(operators.pop());
+                            if (operators.isEmpty()) break;
+                            o = operators.peek();
+                        }
+                    }
+                    operators.push("-");
+                }
+                continue;
+            }
+
+            // Handle multiplication.
+            if (expression[i] == '*') {
+                if (!operators.isEmpty()) {
+                    String o = operators.peek();
+                    while (o.equals("*") || o.equals("/") || o.equals("^")) {
+                        output.add(operators.pop());
+                        if (operators.isEmpty()) break;
+                        o = operators.peek();
+                    }
+                }
+                operators.push("*");
+                continue;
+            }
+
+            // Handle division.
+            if (expression[i] == '/') {
+                if (!operators.isEmpty()) {
+                    String o = operators.peek();
+                    while (o.equals("*") || o.equals("/") || o.equals("^")) {
+                        output.add(operators.pop());
+                        if (operators.isEmpty()) break;
+                        o = operators.peek();
+                    }
+                }
+                operators.push("/");
+                continue;
+            }
+
+            // Handle exponents.
+            if (expression[i] == '^') {
+                operators.push("^");
+                continue;
+            }
+
+            // Functions //
+
+            // If char is a letter, get three-char function name and check if valid.
+            if (Character.isLetter(expression[i])) {
+                // Throw error if there is not enough room for a function name.
+                if (expression.length - i < 3) {
+                    throw new IllegalArgumentException("Invalid Input - Incorrect function name at end of expression."
+                            + " Valid functions are sin(), cos(), tan(), cot(), log(), and ln().");
+                }
+
+                String token = "" + expression[i] + expression[i + 1] + expression[i + 2];
+                switch (token.toLowerCase()) {
+                    case "sin":
+                        operators.push("sin");
+                        break;
+                    case "cos":
+                        operators.push("cos");
+                        break;
+                    case "tan":
+                        operators.push("tan");
+                        break;
+                    case "cot":
+                        operators.push("cot");
+                        break;
+                    case "log":
+                        operators.push("log");
+                        break;
+                    case "ln(":
+                        operators.push("ln");
+                        i--; // ln is shorter than the rest, so offset i.
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid Input - Incorrect function name."
+                                + " Valid functions are sin(), cos(), tan(), cot(), log(), and ln().");
+                }
+                i += 2; // Move i to after the function name.
+                continue;
+            }
+
+            // If character is still not handled, is illegal.
+            throw new IllegalArgumentException("Invalid Input - Incorrect character in expression.");
+        }
+
+        // Move all remaining operators to output, then return.
+        while (!operators.isEmpty()) output.add(operators.pop());
+
+        return output;
+    }
+}
---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -0,0 +1,290 @@
+package com.example.calculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        // Grabs the view from the xml and assigns it to display.
+        display = findViewById(R.id.input);
+
+        //prevents the keyboard from popping up in teh display.
+        display.setShowSoftInputOnFocus(false);
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(getString(R.string.display).equals(display.getText().toString())){
+                    display.setText("");
+                }
+            }
+        });
+    }
+
+    //Updates the string that's on the display
+    private void updateInput (String stringToAdd){
+        //Grabs the string that's already on the display
+        String currString = display.getText().toString();
+
+        //Grabs the cursor position
+        int cursorPosition = display.getSelectionStart();
+
+        //Grabs the left substring up to the cursor
+        String leftSubStr = currString.substring(0, cursorPosition);
+
+        //Grabs the right substring from the cursor position up to the end of the string
+        String rightSubStr = currString.substring(cursorPosition);
+
+        //If the string equals the default text, set the display to the updated text.
+        if (getString(R.string.display).equals(display.getText().toString())){
+            display.setText(stringToAdd);
+
+            //Keeps the cursor at the end of the string instead of the front.
+            display.setSelection(cursorPosition + 1);
+        } else {
+            //Else, update the existing text.
+
+            //Updates the display
+            display.setText(String.format("%s%s%s", leftSubStr, stringToAdd, rightSubStr));
+
+            //Keeps the cursor at the end of the string instead of the front.
+            display.setSelection(cursorPosition + 1);
+        }
+
+    }
+
+    //Numbers
+    public void nineBTN(View view){
+
+        updateInput("9");
+    }
+
+    public void eightBTN(View view){
+
+        updateInput("8");
+    }
+    public void sevenBTN(View view){
+        updateInput("7");
+
+    }public void sixBTN(View view){
+        updateInput("6");
+
+    }public void fiveBTN(View view){
+        updateInput("5");
+
+    }public void fourBTN(View view){
+        updateInput("4");
+
+    }public void threeBTN(View view){
+        updateInput("3");
+
+    }public void twoBTN(View view){
+        updateInput("2");
+
+    }public void oneBTN(View view){
+        updateInput("1");
+
+    }public void zeroBTN(View view){
+        updateInput("0");
+
+    }
+
+    //Functions
+    public void exponentBTN(View view){
+
+        if (display.getText().length() != 0 && display.getSelectionStart() !=0) {
+            updateInput("^");
+        }
+    }
+    public void sineBTN(View view){
+
+        int cursorPosition = display.getSelectionStart();
+
+        updateInput("sin");
+
+        display.setSelection(cursorPosition+3);
+
+    }
+    public void cosineBTN(View view){
+        int cursorPosition = display.getSelectionStart();
+
+        updateInput("cos");
+
+        display.setSelection(cursorPosition+3);
+
+    }
+    public void tangentBTN(View view){
+        int cursorPosition = display.getSelectionStart();
+
+        updateInput("tan");
+
+        display.setSelection(cursorPosition+3);
+
+
+    }
+
+    public void cotangentBTN(View view){
+        int cursorPosition = display.getSelectionStart();
+
+        updateInput("cot");
+
+        display.setSelection(cursorPosition+3);
+
+
+    }
+
+    public void logBTN(View view){
+        int cursorPosition = display.getSelectionStart();
+
+        updateInput("log");
+
+        display.setSelection(cursorPosition+3);
+
+
+    }
+    public void naturalLogBTN(View view){
+        int cursorPosition = display.getSelectionStart();
+
+        updateInput("ln");
+
+        display.setSelection(cursorPosition+2);
+
+
+    }
+
+    public void curlyBracesBTN(View view){
+
+
+
+    }
+    public void clearBTN(View view){
+        display.setText("");
+
+    }
+    public void plusMinusBTN(View view){
+        //Grabs the cursor position
+        int cursorPosition = display.getSelectionStart();
+
+        String textToEdit = display.getText().toString();
+
+        int strLength = display.getText().length();
+
+
+        if(strLength !=0) {
+            display.setText(new StringBuilder().append("-").append(textToEdit).toString());
+            display.setSelection(cursorPosition + 1);
+        }
+    }
+    public void percentBTN(View view){
+        updateInput("%");
+    }
+    public void divisionBTN(View view){
+        updateInput("/");
+    }
+    public void multiplicationBTN(View view){
+        updateInput("*");
+    }
+    public void subtractionBTN(View view){
+        updateInput("-");
+    }
+    public void additionBTN(View view){
+        updateInput("+");
+    }
+
+    public void equalsBTN(View view){
+        Evaluator exp = new Evaluator();
+
+        String expression1 = display.getText().toString();
+
+        char[] expression2 = expression1.toCharArray();
+
+       double result = exp.calculate(expression2);
+
+       String StrResult = String.valueOf(result);
+
+       display.setText(StrResult);
+
+    }
+    public void parenthesesBTN(View view){
+        //Grabs the cursor position
+        int cursorPosition = display.getSelectionStart();
+        int strLength = display.getText().toString().length();
+
+        int numOpenPars = 0;
+        int numClosedPars = 0;
+
+        for (int i = 0; i < cursorPosition; i++){
+
+            if(display.getText().toString().substring(i, i+1).equals("(")) {
+                numOpenPars++;
+            }
+            else if(display.getText().toString().substring(i,i+1).equals(")")) {
+                numClosedPars++;
+            }
+        }
+
+        if (numClosedPars == numOpenPars || display.getText().toString().substring(strLength-1, strLength).equals("(")){
+            updateInput("(");
+        }
+
+        else if (numClosedPars < numOpenPars && !display.getText().toString().substring(strLength-1, strLength).equals("(")){
+            updateInput(")");
+        }
+
+        display.setSelection(cursorPosition + 1);
+
+    }
+    public void pointBTN(View view){
+        updateInput(".");
+    }
+
+    public void backSpaceBTN(View view){
+
+        //Grabs the string to edit
+        String textToEdit = display.getText().toString();
+
+        //Length of the string on display.
+        int length = textToEdit.length();
+
+        //Grabs the cursor position
+        int cursorPosition = display.getSelectionStart();
+
+
+        if (length != 0 && cursorPosition !=0){
+            SpannableStringBuilder position = (SpannableStringBuilder) display.getText();
+            position.replace(cursorPosition-1, cursorPosition, "");
+            display.setText(position);
+            display.setSelection(cursorPosition-1);
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
